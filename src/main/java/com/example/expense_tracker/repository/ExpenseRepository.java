9ba98bc0065a6deb7a09b0a83307a58a04d492b3
lmesi <lszl.emese@gmail.com>
2024-07-04 18:53:55 +0200

    feat: add Expense repository

---
+++ src/main/java/com/example/expense_tracker/repository/ExpenseRepository.java
@@ -0,0 +1,10 @@
+package com.example.expense_tracker.repository;
+
+import com.example.expense_tracker.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByUserId(Long userId);
+}
